89a0b09f5b396a58cd9b17f4511d825fc3cc6d4b
Shiori Nakama <nakamashiori717@gmail.com>
2015-05-08 10:48:49 +0900

    [add] initial commit

--- SampleKeyboadMike/ViewController.m
+++ SampleKeyboadMike/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>{
+    IBOutlet UITextField *textfield;
+    IBOutlet UILabel *label_question;
+    IBOutlet UILabel *label;
+    IBOutlet UILabel * label_seikai;
+    NSArray *questions;
+    NSString *question_current;
+}
 
 @end
 
@@ -17,6 +24,38 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    questions =  @[@"hello",
+                 @"how are you",
+                 @"i'm fine thank you"];
+    int ran = arc4random()%questions.count;
+    question_current = questions[ran];
+    label_question.text = questions[ran];
+    textfield.delegate = self;
+}
+
+- (BOOL)textField:(UITextField *)textField
+shouldChangeCharactersInRange:(NSRange)range
+replacementString:(NSString *)string{
+    label.text = string;
+    [textfield resignFirstResponder];
+    return YES;
+}
+
+-(void)textFieldDidEndEditing:(UITextField *)textField{
+    if ([label.text isEqualToString:question_current]) {
+        label_seikai.text = @ "Great!";
+        //NextQuestion
+        int ran = arc4random()%questions.count;
+        question_current = questions[ran];
+        label_question.text = questions[ran];
+    }else{
+        label_seikai.text = @ "Try again!";
+    }
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField*)textField{
+    [textfield resignFirstResponder];
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning {
