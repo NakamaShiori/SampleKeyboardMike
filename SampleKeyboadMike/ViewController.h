48a38459f93354d9344ad6c43f4eab0d6bcd32ca
Shiori Nakama <nakamashiori717@gmail.com>
2015-05-08 09:09:11 +0900

    Initial Commit

---
+++ SampleKeyboadMike/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleKeyboadMike
+//
+//  Created by N on 2015/05/08.
+//  Copyright (c) 2015年 Nakama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleKeyboadMike/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SampleKeyboadMike
+//
+//  Created by N on 2015/05/08.
+//  Copyright (c) 2015年 Nakama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
