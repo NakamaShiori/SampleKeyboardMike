0f7e93041aa1e3b3c7cf3bc2c666ccc0f85ccb59
Shiori Nakama <nakamashiori717@gmail.com>
2015-05-16 21:24:09 +0900

    [modify] 画面のデザイン一部変更

--- SampleKeyboadMike/ViewController.m
+++ SampleKeyboadMike/ViewController.m
@@ -24,6 +24,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
     questions =  @[@"hello",
                  @"how are you",
                  @"i'm fine thank you"];
